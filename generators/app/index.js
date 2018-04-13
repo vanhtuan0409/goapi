37ae43a35350f43b98a18c62a19198c463c4735e
Tuấn Vương <vanhtuan0409@gmail.com>
2018-04-13 12:44:53 +0700

    init url root

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,11 +1,25 @@
 const Generator = require("yeoman-generator");
+const path = require("path");
 
 module.exports = class GoAPIGenerator extends Generator {
   constructor(args, opts) {
     super(args, opts);
+    this.rootUrl = this._getRepoUrl();
   }
 
   method1() {
-    this.log("Method 1 just ran");
+    this.log(this.rootUrl);
+  }
+
+  _getRepoUrl() {
+    const destinationPath = process.env.LOCAL_PATH || this.destinationRoot();
+    const gopath = process.env.GOPATH;
+    const shouldRemovePath = `${gopath}${path.sep}src`;
+    let repoUrl = "";
+    if (destinationPath.includes(shouldRemovePath)) {
+      repoUrl = destinationPath.replace(shouldRemovePath, "");
+      repoUrl = repoUrl[0] === path.sep ? repoUrl.substr(1) : repoUrl;
+    }
+    return repoUrl;
   }
 };
