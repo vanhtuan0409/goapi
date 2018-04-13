b0659d287b08f4ffcad29ab67132e44884b4d1b9
Tuấn Vương <vanhtuan0409@gmail.com>
2018-04-13 13:18:31 +0700

    init sample

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,11 +4,41 @@ const path = require("path");
 module.exports = class GoAPIGenerator extends Generator {
   constructor(args, opts) {
     super(args, opts);
-    this.rootUrl = this._getRepoUrl();
   }
 
-  method1() {
-    this.log(this.rootUrl);
+  initializing() {
+    this.rootUrl = this._getRepoUrl();
+    this.appName = "";
+  }
+
+  prompting() {
+    if (this.rootUrl == "") {
+      this.log("YoGoAPI can only generate project within GOPATH");
+      return;
+    }
+
+    return this.prompt([
+      {
+        type: "input",
+        name: "appName",
+        message: "Your project name:",
+        validate: function(input) {
+          if (!input) return "Project name is required";
+          return true;
+        }
+      }
+    ]).then(answer => {
+      this.appName = answer.appName;
+    });
+  }
+
+  writing() {
+    if (this.rootUrl == "") return;
+
+    this.fs.copyTpl(
+      this.templatePath("_main.go"),
+      this.destinationPath(`./${this.appName}/main.go`)
+    );
   }
 
   _getRepoUrl() {
